95ec9143ea87c1d57991a2d61721373b6d2fd606
devdutt6 <devduttchudasama1339@gmail.com>
2023-02-12 18:16:55 +0530

    optional route

---
+++ src/routes/[lang]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from '$app/stores';
+
+  let { lang='en' } = $page.params;
+
+  const greetings = {
+    en: "Hello",
+    fr: "Bonjour",
+    sp: "Hola"
+  }
+
+  const greeting = greetings[lang];
+
+</script>
+
+<h2>{greeting}</h2>
